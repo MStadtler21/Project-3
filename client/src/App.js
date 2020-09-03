bcdbb8ddd11dd934c02f48e1a883efe95990fa40
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-09-02 20:19:14 -0600

    Building authentication

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,23 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Todos from "./pages/Todos";
 import NoMatch from "./pages/NoMatch";
+import Navbar from "./components/Navbar";
+import Loading from "./components/Loading";
+import { useAuth0 } from "@auth0/auth0-react";
 // import LoginButton from "./components/LoginButton";
 // import LogoutButton from "./components/LogoutButton";
 
 
-function App() {
+const App = () => {
+
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <Loading />
+  }
   return (
     <Router>
+      <Navbar />
       <div>
         <Switch>
           <Route exact path={["/", "/todos"]}>

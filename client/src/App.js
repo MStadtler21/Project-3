cc9e955ce27c93c9b168c85d0520c1e44bb1c5de
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-09-03 20:06:06 -0600

    Setting up hash router and beginning styling.

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Todos from "./pages/Todos";
-import NoMatch from "./pages/NoMatch";
-import Navbar from "./components/Navbar";
-import Loading from "./components/Loading";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import Footer from "./components/Footer/index"
+import NoMatch from "./pages/NoMatch";
 import ExternalApi from "../src/pages/ExternalApi";
 import history from "../src/utils/history";
-// import LoginButton from "./components/LoginButton";
-// import LogoutButton from "./components/LogoutButton";
-import Styles from "./styles.css"
 
+import Navbar from "./components/Navbar";
+import Loading from "./components/Loading";
+import Sidebar from "./components/Sidebar";
+import Welcome from "./pages/Welcome";
+import Footer from "./components/Footer/index"
+import Todos from "./pages/Todos";
+import Profile from "./pages/Profile";
+
+import Styles from "./styles.css";
 
 const App = () => {
 
@@ -26,21 +28,17 @@ const App = () => {
   }
   return (
     <Router history={history}>
-      <Navbar />
-      <div style={{backgroundColor: "#f1f1e8"}}>
-        <Switch>
-          <Route exact path={["/", "/todos"]}>
-            <Todos />
-          </Route>
-          <Route exact path="/todos/:id">
-          </Route>
-          <Route>
-            <NoMatch />
-          </Route>
-          <Route path="/external-api" component={ExternalApi} />
-        </Switch>
-        <Footer></Footer>
-      </div>
+      <Navbar className="navbar" />
+        <body>
+          <main>
+          <Router>
+              <Route exact path="/" component={Welcome} />
+              <Route exact path="/orders" component={Todos} />
+              <Route path="/profile" component={Profile} />
+          </Router>
+          </main>
+        </body>
+      <Footer />
     </Router>
   );
 }
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { text } from "body-parser";
+import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="footer" style={{backgroundColor: "#bfdcae", marginTop: 35, height: 250, textAlign: "center"}}>
+    <div className="footer">
       <a href="https://github.com/hamery93/Project-3">GitHub</a>
-      <br></br>
       Contributors:
-      <div style={{listStyle:"none", textAlign:"center", margin: 0}}>
+      <div>
            <li>Andrew Kepson</li>
            <li>Hisham Amery </li>
            <li>Matt Stadtler</li>
            <li>Sam Morris</li>
            <li>Travis Martinez</li>
-           Copyright 2020 ©
+           © 2020
       </div>
     </div>
   );

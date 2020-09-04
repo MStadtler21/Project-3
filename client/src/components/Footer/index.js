15f911b0fe3287f5b1c2758a5acb865ac1ecf2f4
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-09-04 11:36:24 -0600

    Resolving view heights

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,18 +5,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      
-      <div>
       View Source Code: <GithubIcon />
-      <br></br>
-      Contributors:
-           <li>Andrew Kepson</li>
-           <li>Hisham Amery </li>
-           <li>Matt Stadtler</li>
-           <li>Sam Morris</li>
-           <li>Travis Martinez</li>
-           © 2020
-      </div>
     </div>
   );
 }

9d68cfc0561ac3248c94bba87c46459464405b5f
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-09-05 09:26:55 -0600

    Added PDF functionality.

--- client/src/pages/ListBuilder.js
+++ client/src/pages/ListBuilder.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./ListBuilder.css";
 import DragNDrop from "../components/DragNDrop";
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
 
 const data = [
     {title: 'group 1', items: ['1', '2', '3']},
@@ -9,9 +11,14 @@ const data = [
 
 const ListBuilder = () => {
     return (
-          <header className="Drag-header">
+        <>
+        <Pdf targetRef={ref} filename="new-order.pdf">
+            {({ toPdf}) => <button onClick={toPdf}> Generate Pdf</button> }
+        </Pdf>
+          <header ref={ref} className="Drag-header">
              <DragNDrop data={data} />
           </header>
+        </>
     );
 }
 

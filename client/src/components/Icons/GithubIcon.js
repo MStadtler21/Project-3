287976760aac860cdd10420345086c0ce9d8c19a
Boba Fett <60700164+AndrewKepson@users.noreply.github.com>
2020-09-06 09:52:24 -0600

    Fixed overflow issue, added browserrouter.

--- client/src/components/Icons/GithubIcon.js
+++ client/src/components/Icons/GithubIcon.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Github } from '@icons-pack/react-simple-icons';
  
 function GithubIcon() {
-  return <a href="https://github.com/hamery93/Project-3" target="_blank" rel="noopener noreferror"><Github color="rgb(0, 0, 0)" size={24} /></a>;
+  return <a href="https://github.com/hamery93/Project-3" rel="noopener noreferror" target="_blank"><Github color="rgb(0, 0, 0)" size={24} /></a>;
 }
 
 export default GithubIcon;
